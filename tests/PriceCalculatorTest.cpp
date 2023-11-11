ac563afdee13ae261dde93eceda7a0b436c449f1
nivag8899 <761768812@qq.com>
2023-11-11 16:57:09 +0800

    改了错别字

--- tests/PriceCalculatorTest.cpp
+++ tests/PriceCalculatorTest.cpp
@@ -2,7 +2,7 @@
 
 #include "PriceCalculator.h"
 
-TEST(PriceCalculator, should_return_100_when_gien_cash_normal_and_price_100)
+TEST(PriceCalculator, should_return_100_when_gvien_cash_normal_and_price_100)
 {
     //given
     PriceCal::PriceCalculator priceCalculator;
@@ -14,7 +14,7 @@ TEST(PriceCalculator, should_return_100_when_gien_cash_normal_and_price_100)
     EXPECT_EQ(100, cash);
 }
 
-TEST(PriceCalculator, should_return_90_when_gien_cash_percentoff_and_price_100)
+TEST(PriceCalculator, should_return_90_when_given_cash_percentoff_and_price_100)
 {
     //given
     PriceCal::PriceCalculator priceCalculator;
@@ -26,7 +26,7 @@ TEST(PriceCalculator, should_return_90_when_gien_cash_percentoff_and_price_100)
     EXPECT_EQ(90, cash);
 }
 
-TEST(PriceCalculator, should_return_80_when_gien_cash_back_and_price_100)
+TEST(PriceCalculator, should_return_80_when_given_cash_back_and_price_100)
 {
     //given
     PriceCal::PriceCalculator priceCalculator;
@@ -38,7 +38,7 @@ TEST(PriceCalculator, should_return_80_when_gien_cash_back_and_price_100)
     EXPECT_EQ(80, cash);
 }
 
-TEST(PriceCalculator, should_return_90_when_gien_cash_percentoff_and_price_90)
+TEST(PriceCalculator, should_return_90_when_given_cash_percentoff_and_price_90)
 {
     //given
     PriceCal::PriceCalculator priceCalculator;
