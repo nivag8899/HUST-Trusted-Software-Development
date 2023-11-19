5ee11a7791c24604f7a65280c9e4eaba495eadc5
Xi Gao <761768812@qq.com>
2023-11-19 16:04:13 +0800

    用unordered_map替换switch_case

--- src/PriceCalculator.cpp
+++ src/PriceCalculator.cpp
@@ -1,54 +1,19 @@
 #include "PriceCalculator.h"
 
-#include <cmath>
-#include <memory>
+
+
 #include <unordered_map>
 
 namespace PriceCalc
 {
     double PriceCalculator::AcceptCash(const DiscountType discountType, const double money) const noexcept
     {
-        double cash = 0.0;
-        std::unique_ptr<Discount> discount;
-
-        switch (discountType)
-        {
-            case DiscountType::CASH_NORMAL:
-            {
-                discount = std::make_unique<Normal>();
-                // cash = discount->AcceptCash(money);
-                break;
-            }
-
-            case DiscountType::CASH_PERCENTOFF_10:
-            {
-                discount = std::make_unique<PercentOff>(0.9);
-                // cash = discount->AcceptCash(money);
-                break;
-            }
-
-            case DiscountType::CASH_PERCENTOFF_20:
-            {
-                discount = std::make_unique<PercentOff>(0.8);
-                // cash = discount->AcceptCash(money);
-                break;
-            }
-
-            case DiscountType::CASH_PERCENTOFF_30:
-            {
-                discount = std::make_unique<PercentOff>(0.7);
-                // cash = discount->AcceptCash(money);
-                break;
-            }
-
-            case DiscountType::CASH_BACK:
-            {
-                discount = std::make_unique<CashBack>();
-                // cash = discount->AcceptCash(money);
-                break;
-            }
-        }
-        cash = discount->AcceptCash(money);
-        return cash;
+        std::unordered_map<DiscountType,std::unique_ptr<Discount>> discountMap;
+        discountMap.emplace(DiscountType::CASH_NORMAL,std::make_unique<Normal>());
+        discountMap.emplace(DiscountType::CASH_PERCENTOFF_10,std::make_unique<PercentOff>(0.9));
+        discountMap.emplace(DiscountType::CASH_PERCENTOFF_20,std::make_unique<PercentOff>(0.8));
+        discountMap.emplace(DiscountType::CASH_PERCENTOFF_30,std::make_unique<PercentOff>(0.7));
+        discountMap.emplace(DiscountType::CASH_BACK,std::make_unique<CashBack>());
+        return discountMap.find(discountType)->second->AcceptCash(money);
     }
 } // namespace PriceCalc
