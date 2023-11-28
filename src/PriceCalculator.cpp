25d5b5184b9430f25286aa0043e7c992ab1eceb7
Xi Gao <761768812@qq.com>
2023-11-28 23:18:44 +0800

    delete useless code blocks.

--- src/PriceCalculator.cpp
+++ src/PriceCalculator.cpp
@@ -1,25 +1,4 @@
 #include "PriceCalculator.h"
-#include "Discount.h"
-#include <unordered_map>
-
-/*
-namespace PriceCalc
-{
-    double PriceCalculator::AcceptCash(const DiscountType discountType, const double money) const noexcept
-    {
-        std::unordered_map<DiscountType,std::unique_ptr<Discount>> discountMap;
-        discountMap.emplace(DiscountType::CASH_NORMAL,std::make_unique<Normal>());
-        discountMap.emplace(DiscountType::CASH_PERCENTOFF_10,std::make_unique<PercentOff>(0.9));
-        discountMap.emplace(DiscountType::CASH_PERCENTOFF_20,std::make_unique<PercentOff>(0.8));
-        discountMap.emplace(DiscountType::CASH_PERCENTOFF_30,std::make_unique<PercentOff>(0.7));
-        discountMap.emplace(DiscountType::CASH_BACK,std::make_unique<CashBack>());
-        return discountMap.find(discountType)->second->AcceptCash(money);
-    }
-} // namespace PriceCalc
-
-*/
-
-#include "PriceCalculator.h"
 #include "DiscountManager.h"  // Add this line
 #include "Discount.h"
 #include <unordered_map>
@@ -32,7 +11,5 @@ namespace PriceCalc {
     }
 } // namespace PriceCalc
 
-
-/*************************************************************/
 
 
